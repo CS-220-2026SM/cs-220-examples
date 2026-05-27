77b9182fc3ea44b71c765e7da766566fb6f9fe5f
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-05-27 13:29:53 -0400

    updated example

--- hello/hello.c
+++ hello/hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(){
-  // TODO: print "Hello, world!"
+int main(int argc,char *argv[]){
+  /* argv[0] = ./hello
+     argv[1] = argument 1
+     argv[2] = arguemnt 2
+     ...
+  */
+  // print "Hello, world!"
+  for (int i = 1; i < argc; i++){
+    printf("Hello, %s!\n", argv[i]);
+  }
   return 0;
 }
