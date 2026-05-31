96c0b9fc0b9d040fe2ae8ed096a732be5a9d8df4
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-05-31 14:50:16 -0400

    new example

---
+++ static/count.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void foo(){
+  // TODO: Print the number of times the function has been called
+}
+
+int main(int argc, char *argv[]){
+  // TODO: take a number as a command line argument and
+  // call foo() that many times
+  return 0;
+}
