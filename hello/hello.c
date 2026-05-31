10f368ae01ab0fe9ea59f9f7c254c2c0c28a054e
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-05-31 14:39:14 -0400

    updated example

--- hello/hello.c
+++ hello/hello.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
+int a = 3;
+int b;
+
+
 int main(int argc,char *argv[]){
   /* argv[0] = ./hello
      argv[1] = argument 1
      argv[2] = arguemnt 2
      ...
   */
-  // print "Hello, world!"
-  for (int i = 1; i < argc; i++){
-    printf("Hello, %s!\n", argv[i]);
+  while (b < a){
+    for (int i = 1; i < argc; i++){
+      printf("Hello, %s!\n", argv[i]);
+    }
+    b++;
   }
   return 0;
 }
