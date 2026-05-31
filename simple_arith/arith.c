eb0be7d25629cee0ec6fcd8b924d23174a63244d
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-05-31 11:39:50 -0400

    added example

---
+++ simple_arith/arith.c
@@ -0,0 +1,18 @@
+int add(int a, int b){
+  return 0; 
+}
+int sub(int a, int b){
+  return 0; 
+}
+int mul(int a, int b){
+  return 0; 
+}
+int divide(int a, int b){
+  return 0; 
+}
+int remain(int a, int b){
+  return 0; 
+}
+int expnt(int a, int b){
+  return 0; 
+}
---
+++ simple_arith/arithTest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arith.h"
+
+int main(int argc, char *argv[]){
+  /* TODO: Get the two integers passed in through the command line
+     and perform each of the operations defined in arith.c,
+     printing the result in the form "1 + 2 = 3".
+     We will need to use the "atoi" function to convert the
+     command line arguments from ascii to integers, and
+     the "printf" function to print our result.
+  */
+  
+  return 0;
+}
