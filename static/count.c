01e3fdb1ebcd1494de19563133428163fb5c7c33
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-06-02 20:42:51 -0400

    updated example

--- static/count.c
+++ static/count.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+#include <stdio.h> // for printf
+#include <stdlib.h> // for atoi
+
 
 void foo(){
-  // TODO: Print the number of times the function has been called
+  // print the number of times the function has been called
+  static int counter = 0;
+  counter++;
+  printf("Run %d times\n", counter);
 }
 
 int main(int argc, char *argv[]){
-  // TODO: take a number as a command line argument and
+  // take a number as a command line argument and
   // call foo() that many times
+  int num = atoi(argv[1]); // ascii to int
+  for (int i = 0; i < num; i++){
+    foo();
+  }
   return 0;
 }
