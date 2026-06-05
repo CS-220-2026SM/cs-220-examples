26bb17cd6b21bed112d1cf23b20c660d0cd63b34
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-06-05 14:02:56 -0400

    added examples

---
+++ swap/swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void swap(int a, int b);
+
+int main(){
+  int a = 100;
+  int b = 200;
+
+  printf("a = %d, b = %d\n",a,b);
+  swap(a,b);
+  printf("a = %d, b = %d\n",a,b);
+
+  return 0;
+}
+
+void swap(int a, int b){
+  // TODO: swap a and b
+}
