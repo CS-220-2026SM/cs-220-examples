78c3b40055d75af1174da3534058789519b3d99c
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-06-05 14:40:33 -0400

    added example

---
+++ structs/id.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct person {
+  char name[20];
+  int age;
+};
+
+void printPerson(struct person p){
+  // TODO: print each field in the struct
+}
+
+// TODO: write a constructor
+struct person *createPerson(char *name, int age){
+
+  return NULL; // change this
+}
+
+int main(){
+  // TODO: initialize three person structs in three different ways
+  
+  return 0;
+}
