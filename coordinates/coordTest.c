687b0d8aa4b89644dba05370dbb8ddcaf84b1afa
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-06-10 14:40:33 -0400

    added example

---
+++ coordinates/coordTest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "coord.h"
+
+int main(int argc, char * argv[]){
+  /* TODO: create a new coordinate pair, whose x- and y-coordinates
+  are specified in the first and second command line arguments resp.
+  then print the value of each coordinate to verify that they were
+  initialized correctly
+  */
+  int x = atoi(argv[1]);
+  int y = atoi(argv[2]);
+  
+  return 0;
+}
