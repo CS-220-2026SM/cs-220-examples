9ccea9b78a8dcf520bbead852fe396a6adae4135
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-06-16 15:39:34 -0400

    added example

--- lowercase/lower.c
+++ lowercase/lower.c
@@ -4,20 +4,35 @@
 
 void toLower(char str[]){
   // TODO: convert upper case letters in str to lower case in-place
+  // recall: 'A' = 0x41, 'Z' = 0x5a; 'a' = 0x61, 'z' = 0x7a
+  int len = strlen(str);
+  for (int i = 0; i < len; i++){
+    if (str[i] >= 0x41 && str[i] <= 0x5a)
+      str[i] += 0x20;
+  }
 }
 
 char *toLowerCpy(char str[]){
   // TODO: make a copy of str, convert upper case -> lower case, and return a pointer to it
-  return NULL;
+  int len = strlen(str);
+  char *newStr = malloc(len*sizeof(char)+1);
+  strcpy(newStr,str);
+  for (int i = 0; i < len; i++){
+    if (newStr[i] >= 0x41 && newStr[i] <= 0x5a)
+      newStr[i] += 0x20;
+  }
+  return newStr;
 }
 
 int main(){
-  char *str1 = "Hello, World!";
-  printf("BEFORE: %s\n", str1);
-  toLower(str1);
-  printf("AFTER: %s\n", str1);
+  char *str1 = "Hello, World!"; // initializing as pointer to string literal
+  char str3[] = "Hello, World!"; // initialing as array on stack
+  printf("BEFORE: %s\n", str3);
+  toLower(str3);
+  printf("AFTER: %s\n", str3);
   printf("ORIGINAL: %s\n", str1);
   char *str2 = toLowerCpy(str1);
   printf("NEW: %s\n", str2);
+  free(str2);
   return 0;
 }
--- union/un.c
+++ union/un.c
@@ -7,5 +7,14 @@ int main(){
     unsigned char bytes[4];
   } num;
   // TODO: print the bytes of num in order
+  num.val = 0x12345678;
+  
+  for (int i = 0; i < 4; i++){
+    printf("num.bytes[%d] = 0x%x\n", i, num.bytes[i]);
+  }
+  num.bytes[0] = 0x99;
+  num.bytes[2] = 0x99;
+  printf("num = 0x%x\n", num.val);
+  printf("sizeof(num) = %ld\n", sizeof(num));
   return 0;
 }
