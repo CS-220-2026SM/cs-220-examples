5c4193b3f26cdb302ee50d7b72b91f00ffcb5329
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-06-18 11:51:07 -0400

    added examples

---
+++ tokens/token.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+  /* TODO: print all tokens in the string str
+     i.e. all non-empty substrings separated by a single
+     space
+  */
+  char str[] = "These are tokens separated by spaces.";
+  
+  return 0;
+}
