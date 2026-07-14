129f0fa758ba3ca7dec14b93b9b61edc227e100e
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-07-14 13:21:12 -0400

    updated examples

--- array_asm/asm_func.s
+++ array_asm/asm_func.s
@@ -1,5 +1,15 @@
 	.global sum_asm
 
 sum_asm:
-
+	# rdi holds a pointer to ary
+	# esi holds the length of 
+	xor %eax, %eax # return val = sum in eax
+	xor %rcx, %rcx # we will use rcx as an index into the array
+	jmp .L0
+.L1:
+	add (%rdi, %rcx, 4), %eax # (base, index, width)
+	inc %rcx
+.L0:
+	cmp %ecx, %esi
+	jg .L1 # jump to loop body if length > index
 	ret
--- lowercase_asm/lower.s
+++ lowercase_asm/lower.s
@@ -1,5 +1,22 @@
 	.global toLower_asm
 
 toLower_asm:
+	# rdi holds a pointer to the string
+	
+	xor %rcx, %rcx
+	jmp .L0
+.L1:
+	cmp $0x41, %al
+	jl .L2
+	cmp $0x5a, %al
+	jg .L2
+	add $0x20, %al
+	mov %al, (%rdi, %rcx, 1)
+.L2:
+	inc %rcx
 
+.L0:
+	mov (%rdi, %rcx, 1), %al
+	test %al, %al
+	jne .L1 # jump to body if not null
 	ret
